d5ee320a402c3a55e91ce16f1706d8e811760014
Tringapps-User20 <vinothkumarp.pukalenthi@tringapps.com>
2023-01-27 17:47:38 +0530

    Initial commit

---
+++ src/main/java/org/example/BasicCreditCard.java
@@ -0,0 +1,11 @@
+package org.example;
+// 3. Design a class for a basic credit card. The class should have the following features:
+//    - Instance variables for the cardholder's name, card number, and expiration date.
+//        - A constructor that allows the user to create a credit card with a given name, card number, and expiration date.
+//        - An equals method that returns true if the given object is a credit card with the same card number, and false otherwise.
+//        - A clone method that creates and returns a new credit card with the same name, card number, and expiration date as the original credit card."
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
